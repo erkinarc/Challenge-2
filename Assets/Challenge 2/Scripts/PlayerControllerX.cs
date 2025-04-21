cabe5daaa7a60339395b09b835466033f9edcf97
Erkin Arıcı <goktanarici@gmail.com>
2025-04-21 14:11:37 +0300

    completed the challenge

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float timeSinceLastDog = 0f;
+    private float dogSpawnDelay = 1.0f;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && timeSinceLastDog < Time.time)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            timeSinceLastDog = Time.time + dogSpawnDelay; // Set the time for the next dog spawn
         }
     }
 }
